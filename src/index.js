49ae81e275a8de6c2be26999b9e78bc4c7d34543
ekarazas <karazas7@gmail.com>
2021-07-07 23:37:11 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send({ error: "Page not found" });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
